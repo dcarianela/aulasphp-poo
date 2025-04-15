29eda74b620431194c8a5fa4bb675e99420ba1af
dcarianela <aki.hayakawa5577@gmail.com>
2025-04-15 11:28:15 -0300

    elimina idade da classe Cliente

--- index.php
+++ index.php
@@ -3,39 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 03</title>
+    <title>Exemplo 04</title>
 </head>
 <body>
-    <h1>PHP com POO - Exemplo 03</h1>
+    <h1>PHP com POO - Exemplo 04</h1>
     <hr>
 
 <?php
 require_once "src/Cliente.php";
 
-$clienteA = new Cliente("Sunoo", 21, "ddeonu@cute.com");
-$clienteB = new Cliente("Ri-ki", 20, "nishimura@gmail.com");
 
 ?>
-    <h2>Acessando/lendo os dados dos objetos</h2>
-    
-    <ul>
-        <li><b>Nome: </b><?=$clienteA->getNome()?></li>
-        <li><b>Idade: </b><?=$clienteA->getIdade()?></li>
-        <li><b>Email: </b><?=$clienteA->getEmail()?></li>
-        <li><b>Faixa etária: </b><?=$clienteA->verificarIdade()?></li>
-    </ul>
-
-    
-    <ul>
-        <li><b>Nome: </b><?=$clienteB->getNome()?></li>
-        <li><b>Idade: </b><?=$clienteB->getIdade()?></li>
-        <li><b>Email: </b><?=$clienteB->getEmail()?></li>
-        <li><b>Faixa etária: </b><?=$clienteB->verificarIdade()?></li>
-    </ul>
-
-    <h2>Testes de acesso aos recursos da classe através do objeto</h2>
-
-    <pre><?=var_dump($clienteA, $clienteB)?></pre>
 
     
 </body>
