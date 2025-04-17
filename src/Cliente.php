d3f0efcb837c4eb8b30d8c74ba3e7995c1d2a4be
dcarianela <aki.hayakawa5577@gmail.com>
2025-04-17 09:22:37 -0300

    implementa situação padrão inicial

--- src/Cliente.php
+++ src/Cliente.php
@@ -4,14 +4,27 @@ class Cliente
 {
     private string $nome;
     private string $email;
+    private Situacao $situacao;
 
     // Método Construtor (sempre é executado automaticamente ao criar objeto)
-    public function __construct(string $nome, string $email)
+    public function __construct(
+        string $nome, string $email, Situacao $situacao = Situacao::INATIVO
+        /* Definindo a situação INATIVO como valor padrão para o parâmetro situacao no construtor */
+        )
     {
         $this->setNome($nome);
         $this->setEmail($email);
+        $this->setSituacao($situacao);
     }
     
+    private function setSituacao(Situacao $situacao):void {
+        $this->situacao = $situacao;
+    }
+
+    public function getSituacao():Situacao {
+        return $this->situacao;
+    }
+
     public function setNome(string $nome): void
     {
         $this->nome = $nome;
